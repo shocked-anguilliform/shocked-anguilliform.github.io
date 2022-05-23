b648f2a336dd6ae91469a5b47706f585fa6ea2fb
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-05-23 01:28:57 -0700

    Improving grade file reading

--- data/js/getgrades.js
+++ data/js/getgrades.js
@@ -9,26 +9,34 @@ async function getCurrentGrades(student) {
 }
 
 function populateCurrentGrades(grades){
-	const semesters = String(grades).split("#S");
+	console.clear();
+	const semesters = String(grades).split("\n#S\n");
 	var semestersCount = semesters.length;
 	for(i = 0; i < semestersCount; i++) {
 		splitSemester(semesters);
 	}
-	document.getElementById("test").innerHTML = semesters;
+	document.getElementById("test").innerHTML = "";
+	let last = semestersCount-1;
+	for(i = 0; i < window['semester'+last].length; i++){
+		document.getElementById("test").innerHTML += window['S'+last+'C'+i] + "<br>";
+	}
 }
 
 function splitSemester(semesters){
 	window['semester'+i] = semesters[i].split("\n");
 	
 	console.log("Split semester " + i);
+	let classes = window['semester'+i].length;
 	console.log("Length: " + window['semester'+i].length);
-	for(j = 0; j < window['semester'+i].length; i++) {
+	for(j = 0; j < classes; j++) {
 		splitClass(i);
 	}
 }
 
-function splitClass(SN) {
-	window['S'+SN+'C'+j] = window['semester'+SN].split(" | ");
-	console.log("Split semester " + i + ", class" + j);
-	console.log("Result (S" + SN + "C" + j + "):" + window['S'+SN+'C'+j]);
+function splitClass(semesterNumber) {
+	let semester = window['semester'+semesterNumber]
+	window['S'+semesterNumber+'C'+j] = semester[j].split(" | ");
+	window['S'+semesterNumber+'C'+j+'name'] = $.trim(window['S'+semesterNumber+'C'+j][0]);
+	console.log(window['S'+semesterNumber+'C'+j+'name']);
+	console.log("Result (S" + semesterNumber + "C" + j + "):" + window['S'+semesterNumber+'C'+j]);
 }
