2288fe7d799f913055708e3791497a2ede637a4d
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-05-22 23:34:03 -0700

    Update getgrades.js

--- data/js/getgrades.js
+++ data/js/getgrades.js
@@ -12,11 +12,18 @@ function populateCurrentGrades(grades){
 	const semesters = String(grades).split("#S");
 	var semestersCount = semesters.length;
 	for(i = 0; i < semestersCount; i++) {
-		splitSemester(i);
+		splitSemester(i, semesters);
 	}
 	document.getElementById("test").innerHTML = semesters;
 }
 
-function splitSemester(number){
+function splitSemester(number, semesters){
+	window['semester'+i] = semesters[i].split("\n");
 	console.log("split semester " + number);
+	console.log("Result: " + window['semester'+i]);
+	console.log("Length: " + window['semester'+i].length);
+}
+
+function splitClass(number) {
+	
 }
