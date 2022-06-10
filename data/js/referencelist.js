f7f16f111bd3bcfa5ddc5f1fd4b5e0deddfa9c2a
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-06-09 22:50:32 -0700

    Add reference list

---
+++ data/js/referencelist.js
@@ -0,0 +1,44 @@
+function reveal(source) {
+  let children = source.children;
+  const parts = {};
+  parts.name = children[0].children[0].innerHTML;
+  parts.picture = children[1].children[0].outerHTML;
+  parts.text = children[1].children[1].innerHTML;
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("blackout").style.display = "block";
+  document.getElementById("variableImage").innerHTML = parts.picture;
+  document.getElementById("variableName").innerHTML = parts.name;
+  document.getElementById("variableText").innerHTML = parts.text;
+}
+
+function hidePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("blackout").style.display = "none";
+}
+
+async function getListItems(list) {
+  let listObject = await fetch("/data/reference/" + list + ".txt");
+  let listText = await listObject.text();
+  addEntries(listText);
+}
+
+function addEntries(entryFile){
+	const entries = String(entryFile).split("\n");
+	var entriesCount = entries.length;
+	for(i = 0; i < entriesCount; i++) {
+		splitEntry(entries);
+	}
+	//display code
+}
+
+function splitEntry(entries){
+	const entry = entries[i].split("|");
+	content = writeHTML(entry[0], entry[1], entry[2]);
+	document.getElementById("entryBox").innerHTML += content;
+}
+
+function writeHTML(name, picture, article) {
+	content = '<div class="entryContainer" onclick="reveal(this)">\n<div class="entry">\n<a>' + name + '</a>\n</div>\n<div class="tooltip">\n<img src="/data/ReferenceList/' + picture + '" alt="' + name + '">\n\<div class="innerUp">\n' + article + '\n</div>\n</div>\n</div>'
+	console.log(content);
+	return content;
+}
