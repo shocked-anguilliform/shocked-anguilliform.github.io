5c25066e0d3ee77f2fde275c553fcb1cc313db9e
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-06-25 23:18:19 -0700

    Update maps.js

--- data/js/maps.js
+++ data/js/maps.js
@@ -30,13 +30,20 @@ function appendEntries(locations) {;
 			}
 			let oldOHTML = entry.outerHTML;
 			entry.outerHTML = '<a href="/locations?item=' + entry.id + '">\n' + oldOHTML + '\n</a>'
-			entry.onmouseover = function() {
+			/*entry.onmouseover = function() {
 				console.log("mouse on");
 				d3.select(this.parentNode).raise();
 				console.log(d3.select(this.parentNode));
 				d3.select(this.parentNode.parentNode).raise();
 				console.log(d3.select(this.parentNode.parentNode));
-			};
+			};*/
+			object.addEventListener("mouseover", 
+				console.log("mouse on");
+				d3.select(this.parentNode).raise();
+				console.log(d3.select(this.parentNode));
+				d3.select(this.parentNode.parentNode).raise();
+				console.log(d3.select(this.parentNode.parentNode));
+			);
 			console.log(entry.id);
 		}
 	}
