510beba863c7993a945fb9a42eeadf883da245ed
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-07-27 13:56:45 -0700

    Update maps.js

--- data/js/maps.js
+++ data/js/maps.js
@@ -6,7 +6,6 @@ function initializeMap(fileName) {
 		console.log(box);
 	});
 	document.body.addEventListener("mousemove", getCursorPosition(event));
-	window.addEventListener("mousemove", getCursorPosition2(event));
 	getLocationFile(fileName);
 }
 
@@ -39,29 +38,25 @@ function appendEntries(locations) {;
 			let oldOHTML = entry.outerHTML;
 			entry.outerHTML = '<a href="/locations?item=' + entry.id + '">\n' + oldOHTML + '\n</a>'
 			let entryA = document.getElementById($.trim(chunks[0])).parentNode;
-			entryA.addEventListener("mouseover", hoverOn);
+			entryA.addEventListener("mouseover", hoverOn(event));
 		}
 	}
 }
 
-function hoverOn() {
+function hoverOn(event) {
 	d3.select(this).raise();
 	d3.select(this.parentNode).raise();
+	let cursorX = event.clientX;
+	let cursorY = event.clientY;
 	let titleBox = document.getElementById("titlePopUp");
 	titleBox.innerHTML = this.children[0].id;
-	titleBox.style.left = window.xCursorPosition;
-	titleBox.style.top = window.yCursorPosition;
+	titleBox.style.left = cursorX;
+	titleBox.style.top = cursorY;
 	titleBox.style.display = "block";
 }
 
 function getCursorPosition(event) {
-  window.xCursorPosition = event.clientX;
-  window.yCursorPosition = event.clientY;
-  console.log("ping");
-}
-
-function getCursorPosition2(event) {
-  window.xCursorPosition = event.clientX;
-  window.yCursorPosition = event.clientY;
-  console.log("pong");
+	window.xCursorPosition = event.clientX;
+	window.yCursorPosition = event.clientY;
+	console.log("ping");
 }
