7d5f0af2df68e8e724c442d3a4d8b20ae7395540
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-07-30 13:21:47 -0700

    trying something else

--- data/js/maps.js
+++ data/js/maps.js
@@ -38,16 +38,14 @@ function appendEntries(locations) {;
 			let oldOHTML = entry.outerHTML;
 			entry.outerHTML = '<a href="/locations?item=' + entry.id + '">\n' + oldOHTML + '\n</a>'
 			let entryA = document.getElementById($.trim(chunks[0])).parentNode;
-			entryA.addEventListener("mouseover", hoverOn(event));
+			entryA.addEventListener("mouseover", hoverOn);
 		}
 	}
 }
 
-function hoverOn(event) {
+function hoverOn() {
 	d3.select(this).raise();
 	d3.select(this.parentNode).raise();
-	let cursorX = event.clientX;
-	let cursorY = event.clientY;
 	let titleBox = document.getElementById("titlePopUp");
 	titleBox.innerHTML = this.children[0].id;
 	titleBox.style.left = cursorX;
