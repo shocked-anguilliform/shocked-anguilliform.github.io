23b8f3f7dffa3f9146b970957ef023c97347dd7c
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2022-09-03 23:45:29 -0700

    Update referencelist.js

--- data/js/referencelist.js
+++ data/js/referencelist.js
@@ -82,7 +82,8 @@ function splitEntry(entries){
 			break;
 		default:
 			console.log("pee");
-			var picture = writePicture($.trim(entry[1]));
+			var picture = "noPicture.jpg";
+			var picture2 = writePicture($.trim(entry[1]));
 			if (entry[3]) {
 				id = $.trim(entry[3]);
 				idHTML = ' id="' + id + '"';
